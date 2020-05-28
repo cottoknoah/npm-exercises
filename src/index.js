d87fa9e0ddf747fd098dc685b5398d9117726f79
Knoah Cotto <cotto.knoah@gmail.com>
2020-05-28 08:12:18 -0500

    mpn work

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const $ = require ('jquery');
+
+const sayHello = (name) => `Hello, ${name}!`;
+console.log(sayHello("Ganymede"));
+
+import!!
+
+
+
+// const getMovies = () => {
+//     //
+//     //
+//     //
+//     return "the movies";
+// };
+//
+// const getMovie = (id) =>  {
+//     return `Movie with id of ${id}`;
+// };
+//
+// const deleteMovie = (id) =>  {
+//     return `Deleting movie with id of ${id}`;
+// };
+//
+// // adding/ editing
+//
+// import {getMovies, getMovie , deleteMovie} from './movies-api';
+//
+// console.log(getMovie());
+// console.log(getMovie());
+// console.log(getMovie());
+// console.log(getMovie());
